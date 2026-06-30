0d07ea1f3abf47cebc4de8dbbf2860a256f110b3
ravinder-webipher <ravi.webipher@gmail.com>
2026-06-30 12:08:58 +0530

    Add files via upload

--- python_project/index1(quiz_program).py
+++ python_project/index1(quiz_program).py
@@ -1,6 +1,5 @@
-#quiz program
-
-print('----------------------------quiz program------------------')
+#personal details
+print('----------------------------personal details------------------')
 name=input("Enter your name ")
 gender=input("Enter your gender ")
 age=int(input("Enter your age "))
@@ -8,10 +7,12 @@ address=input("Enter your address ")
 city=input("Enter your city ")
 state=input("Enter your state ")
 pincode=input("Enter your pin code ")
+print()
 
 #age and password validation
 if age>=18:
-    print("\nAge validation completed. Go to next step")
+    print("Age validation completed. Go to next step")
+    print()
     pwd_correct=False
     tickYes=False
     for i in range(1,4):
@@ -26,7 +27,8 @@ if age>=18:
             
     #Terms and conditions
     if pwd_correct is True:
-        print('\n----------Terms and conditions----------------')
+        print()
+        print('----------Terms and conditions----------------')
         print("1. Participants must provide correct and complete registration details. Please tick Y or N ")
         print("2. Each participant is allowed to register and play only once. ")
         print("3. The quiz must be completed within the specified time limit.  ")
@@ -34,89 +36,137 @@ if age>=18:
         print("5. The organizer reserves the right to disqualify any participant found violating the rules..  ")
         print("6. The organizer's decision regarding scores and winners will be final and binding.  ")
         print("7. The organizer reserves the right to modify, suspend, or cancel the quiz at any time.  ")
-        tick=input("\nPlease click Yes if you accept all the conditions. else press No: ")
+        print()
+        tick=input("Please click Yes if you accept all the conditions. else press No: ")
+        print()
+        
         if tick=='Yes':
-            print("\nEntering game: \n")
+            print("Entering game: ")  
             tickYes=True
         else:
             print("You can't play game.")
+            print()
 
     # #quiz questions
     if tickYes is True:
         cr_ans=0
         print("1. What is Python")
-        print(" a) A web browser\n b) A programming language\n c) An operating system\n d) A database")  
+        print("a) A web browser") 
+        print("b) A programming language")
+        print("c) An operating system")
+        print("d) A database")
+        print()
         for i in range(1,3):
-            answer=input("\nAnswer: ")
+            answer=input("Answer: ")
             if answer=='b':
                 print("Correct answer: b")
                 cr_ans+=1
                 break
             else:
                 print("Incorrect.") 
-        print("\n2. Who developed Python programming language")
-        print(" a) Dennis Ritchie\n b) James Goslingn\n c) Guido van Rossum\n d) Bjarne Stroustrup") 
+
+        print()
+        print("2. Who developed Python programming language")
+        print("a) Dennis Ritchie")
+        print("b) James Goslingn")
+        print("c) Guido van Rossum")
+        print("d) Bjarne Stroustrup") 
+        print()
         for i in range(1,3):
-            answer=input("\nAnswer: ")
+            answer=input("Answer: ")
             if answer=='c':
                 print("Correct answer: c")
                 cr_ans+=1
                 break
             else:
                 print("Incorrect. Try again")
-        print("\n3 What is the extension of python file")
-        print(" a) .python\n b) .pt\n c) .py\n d) .pyt")  
+                
+        print()
+        print("3 What is the extension of python file")
+        print("a) .python")
+        print("b) .py") 
+        print("c) .pt") 
+        print("d) .pyt")
+        print()
         for i in range(1,3):
-            answer=input("\nAnswer: ")
-            if answer=='c':
-                print("Correct answer: c")
-                cr_ans+=1
-                break
-            else:
-                print("Incorrect. Try again")
-        print("\n4. Which function is used to take input from user in python")
-        print(" a) read()\n b) scan()\n c) input()\n d) print()")
-        for i in range(1,3):
-            answer=input("\nAnswer: ")
-            if answer=='c':
-                print("Correct answer: c")
-                cr_ans+=1
-                break
-            else:
-                print("Incorrect. Try again")
-        print("\n5. Which function is used to display output in python")
-        print(" a) read()\n b) scan()\n c) input()\n d) print()")
-        for i in range(1,3):
-            answer=input("\nAnswer: ")
-            if answer=='d':
-                print("Correct answer: d")
-                cr_ans+=1
-                break
-            else:
-                print("Incorrect. Try again")
-        print("\n6. Which symbol is used to write a single-line comment in Python")
-        print(" a) //\n b) #\n c) /* */\n d) --")  
-        for i in range(1,3):
-            answer=input("\nAnswer: ")
+            answer=input("Answer: ")
             if answer=='b':
                 print("Correct answer: b")
                 cr_ans+=1
                 break
             else:
                 print("Incorrect. Try again")
-        print("\n7. Which loop is mostly used in python")
-        print(" a) for loop\n b) while loop\n c) All of the above\n d) None of the above")
+                
+        print()
+        print("4. Which function is used to take input from user in python")
+        print("a) read()")
+        print("b) scan()")
+        print("c) input()") 
+        print("d) print()")
+        print()
         for i in range(1,3):
-            answer=input("\nAnswer: ")
+            answer=input("Answer: ")
+            if answer=='c':
+                print("Correct answer: c")
+                cr_ans+=1
+                break
+            else:
+                print("Incorrect. Try again")
+                
+        print()
+        print("5. Which function is used to display output in python")
+        print("a) read()")
+        print("b) scan()") 
+        print("c) input()") 
+        print("d) print()")
+        print()
+        for i in range(1,3):
+            answer=input("Answer: ")
+            if answer=='d':
+                print("Correct answer: d")
+                cr_ans+=1
+                break
+            else:
+                print("Incorrect. Try again")
+                
+        print()
+        print("6. Which symbol is used to write a single-line comment in Python")
+        print("a) //")
+        print("b) #")
+        print("c) /* */")
+        print("d) --")
+        print()
+        for i in range(1,3):
+            answer=input("Answer: ")
+            if answer=='b':
+                print("Correct answer: b")
+                cr_ans+=1
+                break
+            else:
+                print("Incorrect. Try again")
+                
+        print()
+        print("7. Which loop is mostly used in python")
+        print("a) for loop")
+        print("b) while loop")
+        print("c) All of the above")
+        print("d) None of the above")
+        print()
+        for i in range(1,3):
+            answer=input("Answer: ")
             if answer=='a':
                 print("Correct answer: a")
                 cr_ans+=1
                 break
             else:
                 print("Incorrect. Try again")
+    
         cr_ans_per=cr_ans/7*100
-        print("\nQuiz completed. your score is ",int(cr_ans_per)," percent.")
+        print()
+        print("Quiz completed. your score is ",int(cr_ans_per)," percent.")
+        print()
     
     
 else:
-    print("\nYou are under age. You can't play game.")
+    print("You are under age. You can't play game.")
+    print()
